f6415bb1aaab68bdbb812ec9976dc3ab5d3fca85
ngseke <ngseke@gmail.com>
2023-03-19 16:18:32 +0800

    feat: create web

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import { startListener } from './modules/telegram'
 import { task } from './modules/task'
+import { startWeb } from './modules/web'
 
 startListener()
 
 setInterval(task, 1000 * 60 * 30)
 task()
+
+startWeb()
