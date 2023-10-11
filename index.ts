8ae97a06534a871e6aa14bd104f8d7b9496fb8f9
ngseke <ngseke@gmail.com>
2023-10-11 17:42:01 +0800

    feat: add envs

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { startListener } from './modules/telegram'
 import { task } from './modules/task'
 import { startWeb } from './modules/web'
+import { CRAWL_INTERVAL } from './modules/constants'
 
 startListener()
 startWeb()
 task()
 
-setInterval(task, 1000 * 60 * 10)
+setInterval(task, CRAWL_INTERVAL)
--- modules/constants.ts
+++ modules/constants.ts
@@ -2,3 +2,11 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN ?? ''
+export const FIREBASE_CONFIG = (() => {
+  try {
+    return JSON.parse(process.env.FIREBASE_CONFIG ?? '') ?? {}
+  } catch (e) {
+    return {}
+  }
+})()
+export const CRAWL_INTERVAL = Number(process.env.CRAWL_INTERVAL) || 1000 * 60 * 30
